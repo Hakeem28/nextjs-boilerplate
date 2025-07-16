86ccb5ee165470921611ed45cc8cfae83ade852e
Hakeem28 <140258760+Hakeem28@users.noreply.github.com>
2025-07-15 22:15:30 -0400

    Add .next/static/chunks/src_app_layout_tsx_c0237562._.js 1752632113648

---
+++ .next/static/chunks/src_app_layout_tsx_c0237562._.js
@@ -0,0 +1,9 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/[root-of-the-server]__db31e11a._.css",
+  "static/chunks/node_modules_29e89fff._.js",
+  "static/chunks/src_components_nooriq_LogoMushaf_tsx_93c23654._.js"
+],
+    source: "dynamic"
+});
